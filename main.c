5a6aa84d1829bf744fd907ff2a703f64136feb79
Masataka Hayase <mahayase@c1r23s3.42tokyo.jp>
2023-04-29 17:23:54 +0900

    add input data

--- main.c
+++ main.c
@@ -2,27 +2,12 @@
 #include "mlx.h"
 #include <X11/keysym.h>
 
-t_mappoint	*new_point(double x, double y, double z)
-{
-	t_mappoint	*point;
-
-	point = (t_mappoint *)malloc(sizeof(t_mappoint));
-	if (point == NULL)
-		return (NULL);
-	point->x = x;
-	point->y = y;
-	point->z = z;
-	point->next = NULL;
-	return (point);
-}
-
-int	input_data(t_mappoint *head, char *map_data)
+int	input_data(t_map *map, char *map_data)
 {
 	size_t		i;
 	size_t		j;
 	int			fd;
 	char		*line;
-	t_mappoint	*point;
 
 	i = 0;
 	fd = open(map_data, O_RDONLY);
@@ -34,20 +19,22 @@ int	input_data(t_mappoint *head, char *map_data)
 			break ;
 		while (line[j] == '\n' || line[j] == "\0")
 		{
-			point = new_point(i, j, line[j]);
+			lst_pushback(map, i, j, line[j]);
 			j++;
 		}
+		map->col = j;
 		free(line);
 		i++;
 	}
+	map->row = i;
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_map	*map;
 
 	map = map_init(map);
-	input_data(map->head, av[1]);
+	input_data(map, av[1]);
 	return (0);
 }
